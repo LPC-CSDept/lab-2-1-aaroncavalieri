9248a4e20618869a0e0ef4e1b4edf77bc5415b31
Your Name/Applications/anaconda3/bin/python <you@example.com>
2023-01-31 17:36:36 -0800

    V01

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 def main():
    ##################################################
-   # Comlete your code here
+   original_price = int(input("Orginial Price: "))
+   rate = int(input("Discount Amount: "))
+   discount_amount = (original_price * (rate / 100))
+   final_price = original_price - discount_amount
+   print ("Regular Price: " + "$" + str(original_price))
+   print ("Discount Amount: " + "$" + str(discount_amount))
+   print ("The Final Price: " + "$" + str(final_price))
    ##################################################
 
-    pass
+pass
 
 
 if __name__ == '__main__':
